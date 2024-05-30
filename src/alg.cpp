9ef7ddb2b0fe089f1ad8029de1543b7d681660c2
Peace0death <160275048+Peace0death@users.noreply.github.com>
2024-05-30 18:34:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,29 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include <fstream>
+#include <iostream>
+#include <cctype>
+#include "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    if (!file) {
+        std::cerr << "gg" << std::endl;
+        return BST<std::string>();
+    }
+    char ch;
+    std::string word;
+    BST<std::string> binTree;
+    while (file.get(ch)) {
+        if (std::isalpha(static_cast<unsigned char>(ch))) {
+            word += std::tolower(static_cast<unsigned char>(ch));
+        } else if (!word.empty()) {
+            binTree.add(word);
+            word.clear();
+        }
+    }
+    if (!word.empty()) {
+        binTree.add(word);
+    }
+    file.close();
+    return binTree;
 }
